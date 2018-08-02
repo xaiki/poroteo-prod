9fa06e8f378994db4b2776b179a01ff8b44ace87
Niv Sardi <xaiki@evilgiggle.com>
2018-08-02 20:14:35 -0300

    move to react-scripts
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,13 @@
+import React from 'react'
+
+import lasdesistemas from '../img/logos/lasdesistemas.png'
+
 export default () => (
   <header className='header'>
     <a href='/'>
       <img
         className='lasdesistemas-logo'
-        src='static/assets/img/logos/lasdesistemas.png'
+        src={lasdesistemas}
         alt='[LAS] de sistemas' />
     </a>
     <h1 className='title'><span className='main'>Votación en senadores</span> <span className='sub'>de la ley de aborto legal, seguro y gratuito en Argentina</span></h1>
@@ -42,6 +46,6 @@ export default () => (
           font-size: 20px;
         }
       }
-    `}</style>
+            `}</style>
   </header>
 )
